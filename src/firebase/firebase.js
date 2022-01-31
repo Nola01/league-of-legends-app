757a2604ed3753dbe7d9b1d948b862a3aa6200a2
Juan María Nolasco Romero <juanmanolasco0105@gmail.com>
2022-01-31 21:30:10 +0100

    Added firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYe23tP2MhlkAKpyiC-bhqHLZWNxMMdYg",
+  authDomain: "league-of-legends-app-9ae9f.firebaseapp.com",
+  projectId: "league-of-legends-app-9ae9f",
+  storageBucket: "league-of-legends-app-9ae9f.appspot.com",
+  messagingSenderId: "198610624125",
+  appId: "1:198610624125:web:d34eafa156dd695d129b14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
