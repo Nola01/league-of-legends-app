c2396fee6c82255fc8331c038750feb4508d4837
Nola01 <juanmanolasco0105@gmail.com>
2022-02-12 20:39:13 +0100

    Added Characters route

--- src/routers/MainRouter.js
+++ src/routers/MainRouter.js
@@ -4,6 +4,7 @@ import { PrivateRoute } from './PrivateRoute';
 import Register from '../components/Register';
 import Login  from '../components/Login';
 import Home from '../components/Home';
+import Characters from '../components/Characters';
 
 
 const MainRouter = () => {
@@ -13,8 +14,9 @@ const MainRouter = () => {
                 <Route path = "/login" element = {<PublicRoute><Login/></PublicRoute>}/>
                 <Route path = "/register" element = {<PublicRoute><Register/></PublicRoute>}/>
 
-                <Route path="/" element = {<PrivateRoute><Home/></PrivateRoute>}/> 
-                    
+                <Route path="/" element = {<PrivateRoute><Home/></PrivateRoute>}>
+                    <Route index element = {<PrivateRoute><Characters/></PrivateRoute>}/>
+                </Route> 
             </Routes>
         </BrowserRouter>
     );
