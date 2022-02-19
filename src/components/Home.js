71d166756115f1fe722e5758a63ee0db87b60a79
Nola01 <juanmanolasco0105@gmail.com>
2022-02-19 18:26:55 +0100

    Changed Appbar background color

--- src/components/Home.js
+++ src/components/Home.js
@@ -156,7 +156,7 @@ function Home() {
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="absolute" open={open}>
+        <AppBar position="absolute" open={open} style={{ background: '#0a0a0a' }}>
           <Toolbar
             sx={{
               pr: '24px', // keep right padding when drawer closed
