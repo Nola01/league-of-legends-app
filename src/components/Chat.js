f22fcb39ec64f816739f1eb1d506d74c8d85a976
Nola01 <juanmanolasco0105@gmail.com>
2022-02-21 17:07:15 +0100

    Created chat dialog

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,22 +1,43 @@
-import Avatar from '@mui/material/Avatar';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import EditIcon from '@mui/icons-material/Edit';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { FormControl, FormHelperText, InputLabel, MenuItem, Select, Stack } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom';
+
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
 
 import MessageBox from './MessageBox';
 
 export default function Chat() {
 
+  const [open, setOpen] = useState(true);
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    //setOpen(false);
+    navigate('/');
+  };  
+
   return (
       <div>
-          <MessageBox/>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              Chat no disponible
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                El chat aún no está disponible. ¡Pronto podrás hablar con otros jugadores!
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} autoFocus>
+                Aceptar
+              </Button>
+            </DialogActions>
+          </Dialog>
       </div>
   );
 }
